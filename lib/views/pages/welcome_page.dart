87d32c950de5c8e3f0cb3958523af9422bc47193
ditconhanhphuc <21522021@gm.uit.edu.vn>
2025-03-14 22:59:29 +0700

    set up proj and welcome page

---
+++ lib/views/pages/welcome_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/welcome.json', height: 400.0),
+                const FittedBox(
+                  child: Text(
+                    'Pack Runner',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0,
+                      letterSpacing: 100.0,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                FilledButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) {
+                    //       return const OnboardingPage();
+                    //     },
+                    //   ),
+                    // );
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('Get started'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

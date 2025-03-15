d29c2948d0f0293b49aa91b447fd984e7389348e
ditconhanhphuc <21522021@gm.uit.edu.vn>
2025-03-15 18:28:49 +0700

    feat: login and register page

--- lib/views/pages/signin_page.dart
+++ lib/views/pages/signin_page.dart
@@ -1,4 +1,3 @@
-import 'package:client/views/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 class SigninPage extends StatelessWidget {
@@ -8,15 +7,126 @@ class SigninPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign in Page'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text('Sign In',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Divider(height: 0.5, color: Color(0xFFDCDCDC)),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Sign in'),
-            const SizedBox(height: 20),
-          ],
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your phone number',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot password?',
+                      style: TextStyle(color: Color(0xFF0F698C))),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF0F698C),
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+              SizedBox(height: 100),
+              Row(
+                children: [
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text('Or'),
+                  ),
+                  Expanded(child: Divider()),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Image.asset('assets/images/apple.jpg'),
+                    iconSize: 40,
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                    icon: Image.asset('assets/images/google.jpg'),
+                    iconSize: 40,
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                    icon: Image.asset('assets/images/facebook.jpg'),
+                    iconSize: 40,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Spacer(),
+              Text(
+                'By logging in, you agree to our',
+                style: TextStyle(color: Color(0xFF818181)),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  'Terms of Service & Privacy Policy',
+                  style: TextStyle(
+                      color: Color(0xFF0F698C), fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 60),
+            ],
+          ),
         ),
       ),
     );
